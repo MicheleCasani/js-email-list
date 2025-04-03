ba8c79da4e1f4ee86bcab1808af618cd43963cce
MicheleCasani <casanimichele@gmail.com>
2025-04-03 15:04:28 +0200

    [UPD] fix cicle for

--- js/scipt.js
+++ js/scipt.js
@@ -5,15 +5,17 @@
 
 
 const randomEmail = document.getElementById('random-email')
-let emails;
+let emails = [];
 
-for(let i=0; i < 10; i++){
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
-    let email
-    emails = resp.data.response;
-    console.log(emails)
-    randomEmail.innerHTML = emails
-})
+
+for (let i = 0; i < 10; i++) {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
+        let email = resp.data.response;
+        emails.push(email)
+        if (emails.length === 10)
+            randomEmail.innerHTML = emails
+        console.log(emails)
+    })
 }
 
 
